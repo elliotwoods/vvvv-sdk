8cdcc94d62c16101d7e53593413dff4794ba1ca9
Elliot Woods <elliot.woods@gmail.com>
2011-12-24 13:26:09 +0000

    added emgucv

---
+++ vvvv45/addonpack/src/nodes/plugins/Image/OpenCV/src/User Interfaces/IDestinationInstance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VVVV.Nodes.EmguCV
+{
+	public abstract class IDestinationInstance : IInstance, IInstanceInput, IDisposable
+	{
+		protected CVImageInput FInput;
+
+		virtual public void Initialise() { }
+
+		private bool FNeedsInitialisation = true;
+		virtual public bool NeedsInitialise()
+		{
+			if (FNeedsInitialisation)
+			{
+				FNeedsInitialisation = false;
+				return true;
+			}
+			return false;
+		}
+
+		abstract public void Process();
+		
+		public void SetInput(CVImageInput input)
+		{
+			FInput = input;
+		}
+
+		public bool HasInput(CVImageInput input)
+		{
+			return FInput == input;
+		}
+
+		virtual public void Dispose()
+		{
+
+		}
+	}
+}
