cede63d42878c0896bce302068ecd5ea1644d8f9
Elliot Woods <elliot.woods@gmail.com>
2012-01-05 16:56:54 +0000

    help patch added for VideoInput, VideoInput added to MyPlugins

---
+++ vvvv45/addonpack/src/nodes/plugins/Image/OpenCV/src/Sources/ImageLoadNode.cs
@@ -0,0 +1,86 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using VVVV.Core.Logging;
+using VVVV.PluginInterfaces.V2;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace VVVV.Nodes.OpenCV
+{
+	public class ImageLoadInstance : IGeneratorInstance
+	{
+		string FFilename = "";
+		
+		public override bool NeedsThread()
+		{
+			return false;
+		}
+
+		public string Filename
+		{
+			set
+			{
+				if (FFilename != value)
+				{
+					FFilename = value;
+					LoadImage();
+				}
+			}
+		}
+
+		public void Reload()
+		{
+			LoadImage();
+		}
+
+		private void LoadImage()
+		{
+			try
+			{
+				FOutput.Image.LoadFile(FFilename);
+				FOutput.Send();
+				Status = "OK";
+			}
+			catch
+			{
+				Status = "Image load failed";
+			}
+		}
+	}
+
+	#region PluginInfo
+	[PluginInfo(Name = "ImageLoad", Category = "OpenCV", Help = "Loads RGB texture", Author = "alg", Tags = "")]
+	#endregion PluginInfo
+	public class ImageLoadNode : IGeneratorNode<ImageLoadInstance>
+	{
+		#region fields & pins
+		[Input("Filename", StringType = StringType.Filename, DefaultString = null)] 
+		IDiffSpread<string> FPinInFilename;
+
+		[Input("Reload", IsBang = true)]
+		ISpread<bool> FPinInReload;
+
+		[Import]
+		ILogger FLogger;
+		#endregion fields&pins
+
+		[ImportingConstructor]
+		public ImageLoadNode()
+		{
+
+		}
+
+		protected override void Update(int InstanceCount)
+		{
+			if (FPinInFilename.IsChanged)
+				for (int i = 0; i < InstanceCount; i++)
+					FProcessor[i].Filename = FPinInFilename[i];
+
+			for (int i = 0; i < InstanceCount; i++)
+			{
+				if (FPinInReload[i])
+					FProcessor[i].Reload();
+			}
+		}
+	}
+}
