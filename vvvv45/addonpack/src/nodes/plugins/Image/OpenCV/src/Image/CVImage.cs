26751b85a92237b719095f30df43b62f7483a9de
Elliot Woods <elliot.woods@gmail.com>
2012-01-16 12:38:14 +0000

    added image

---
+++ vvvv45/addonpack/src/nodes/plugins/Image/OpenCV/src/Image/CVImage.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System.Runtime.InteropServices;
+using Emgu.CV.CvEnum;
+
+namespace VVVV.Nodes.OpenCV
+{
+	public class CVImage : ImageBase, IDisposable
+	{
+		IImage FImage;
+
+		public override IImage GetImage()
+		{
+			return FImage;
+		}
+
+		public void Initialise(CVImageAttributes attributes)
+		{
+			Initialise(attributes.Size, attributes.ColourFormat);
+		}
+
+		public bool Initialise(System.Drawing.Size size, TColourFormat format)
+		{
+			bool changedAttributes = FImageAttributes.CheckChanges(format, size);
+
+			if (changedAttributes || this.Allocated == false)
+			{
+				Allocate();
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public void GetImage(TColourFormat format, CVImage target)
+		{
+			if (format == this.NativeFormat)
+				ImageUtils.CopyImage(this, target);
+			else
+				ImageUtils.CopyImageConverted(this, target);
+		}
+
+		public void GetImage(CVImage target)
+		{
+			GetImage(target.ImageAttributes.ColourFormat, target);
+		}
+
+		public unsafe bool SetImage(IImage source)
+		{
+			if (source == null)
+				return false;
+
+			TColourFormat sourceFormat = ImageUtils.GetFormat(source);
+			bool Reinitialise = Initialise(source.Size, sourceFormat);
+
+			ImageUtils.CopyImage(source, this);
+
+			return Reinitialise;
+		}
+
+		public bool SetImage(CVImage source)
+		{
+			if (source == null)
+				return false;
+
+			bool Reinitialise = Initialise(source.Size, source.NativeFormat);
+
+			ImageUtils.CopyImage(source, this);
+
+			return Reinitialise;
+		}
+
+		/// <summary>
+		/// Copy data from pointer. Presume we're initialised and data is of correct size
+		/// </summary>
+		/// <param name="rawData">Raw pixel data of correct size</param>
+		public void SetPixels(IntPtr rawData)
+		{
+			if (rawData == IntPtr.Zero)
+				return;
+
+			ImageUtils.CopyImage(rawData, this);
+		}
+
+		override public void Allocate()
+		{
+			FImage = ImageUtils.CreateImage(this.Width, this.Height, this.NativeFormat);
+		}
+
+		public void LoadFile(string filename)
+		{
+			this.SetImage(new Image<Bgr, byte>(filename));
+		}
+
+		public void Dispose()
+		{
+			if (FImage != null)
+			{
+				FImage.Dispose();
+				FImage = null;
+			}
+		}
+	}
+}
---
+++ vvvv45/addonpack/src/nodes/plugins/Image/OpenCV/src/User Interfaces/IDestinationNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes.OpenCV
+{
+	public abstract class IDestinationNode<T> : IPluginEvaluate, IDisposable where T : IDestinationInstance, new()
+	{
+		[Input("Input", Order = -1)]
+		private ISpread<CVImageLink> FPinInInputImage;
+
+		protected ProcessDestination<T> FProcessor;
+
+		public void Evaluate(int SpreadMax)
+		{
+			if (FProcessor == null)
+				FProcessor = new ProcessDestination<T>(FPinInInputImage);
+
+			FProcessor.CheckInputSize(FPinInInputImage.SliceCount);
+
+			Update(FProcessor.SliceCount);
+		}
+
+		protected abstract void Update(int InstanceCount);
+
+		public void Dispose()
+		{
+			FProcessor.Dispose();
+		}
+	}
+}
