3931f5d6bc7af0170f11049bdead05ef8d1ac22a
Elliot Woods <elliot.woods@gmail.com>
2012-01-18 06:26:42 +0000

    added export projects (will become stale whilst not exporting)

---
+++ vvvv45/addonpack/src/nodes/plugins/Image/OpenCV/src/Process/IProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+using System.Threading;
+
+namespace VVVV.Nodes.OpenCV
+{
+	public abstract class IProcess<T>
+	{
+		protected Spread<T> FProcess = new Spread<T>(0);
+
+		protected Thread FThread;
+		protected bool FThreadRunning = false;
+		protected Object FLockProcess = new Object();
+	}
+}
